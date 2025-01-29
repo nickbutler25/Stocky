3cc21d43e67f9fc0441bbb7be2a8e7a605d08959
Nick Butler <nick.w.butler@gmail.com>
2025-01-28 22:29:04 -0500

    Work with variables and secrets

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 import time as t
-from datetime import datetime, time, timedelta
+from datetime import datetime, timedelta
 import logging
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,11 +10,15 @@ from selenium.webdriver.common.by import By
 from collections import namedtuple
 from selenium.webdriver.support.wait import WebDriverWait
 from times import GenerateTimes
-import asyncio
 import sys
+import json
+
+TIMEOUT = 20  # Timeout for WebDriverWait
+MAX_RETRIES = 30  # Maximum number of retries for finding the day element
+REFRESH_INTERVAL = 0.5  # Time interval between page refreshes (in seconds)
 
 
-async def time_search(username, password, preferred_time):
+def login_and_setup(username, password, time_to_book):
     # Get today's date in UTC (GMT is the same as UTC)
     today = datetime.now().date()
     from datetime import time
@@ -28,28 +32,12 @@ async def time_search(username, password, preferred_time):
     # while datetime.now() < login_time:
     #    t.sleep(40)
 
-    times = GenerateTimes(preferred_time)
+    times = GenerateTimes(time_to_book)
     times_sorted_list, min_time, max_time, preferred_time = times.get_list_of_times()
-    logging.info(f'Searching with a minimum time of {min_time.time()}')
-    logging.info(f'a maximum time of {max_time.time()}')
-    logging.info(f'trying to find a time closest to {preferred_time.time()} as possible')
+    logging.info(f'Trying to book {preferred_time.time()}')
 
     day_to_search = times_sorted_list[0].day
     logging.info(f'On the following date {times_sorted_list[0].date()}')
-
-    # Setup Chrome options
-    #chrome_options = Options()
-    # Optional: run Chrome in headless mode
-
-    # Setup the Chrome service
-    #service = Service(r'C:\Chrome\chromedriver-win64\chromedriver.exe')
-    #service.start()
-
-    # Create the Remote WebDriver with options
-    #driver = webdriver.Remote(
-    #    command_executor=service.service_url,
-    #    options=chrome_options
-    #)
 
     # Use chromedriver
     options = webdriver.ChromeOptions()
@@ -98,114 +86,109 @@ async def time_search(username, password, preferred_time):
     driver.find_element(By.NAME, "Submit").click()
     logging.info(f'Make booking selected')
 
+
     logging.info(f'Waiting for dates to load')
     # Select Date
     WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.LINK_TEXT, str(day_to_search - 1)))
     )
     logging.info(f'Dates loaded')
+    wait_and_book(driver, start_time, day_to_search, time_to_book)
 
-    logging.info(f'Waiting for start time of {start_time.time()} ')
-    # IF TIME >= than start time refresh page wait for day -1 then check for day
+
+def validate_inputs(start_time: datetime, day_to_search: int, time_to_book: str) -> None:
+    """Validate the inputs to ensure they are in the correct format."""
+    if not isinstance(start_time, datetime):
+        raise ValueError("start_time must be a datetime object")
+    if not isinstance(day_to_search, int) or day_to_search < 1 or day_to_search > 31:
+        raise ValueError("day_to_search must be an integer between 1 and 31")
+    if not isinstance(time_to_book, str):
+        raise ValueError("time_to_book must be a string")
+
+def wait_until_start_time(start_time: datetime) -> None:
+    """Wait until the specified start time is reached."""
+    logging.info(f'Waiting for start time of {start_time.time()}')
     while datetime.now() < start_time:
         t.sleep(1)
 
-    elements = None
+def find_and_click_day_element(driver: webdriver, day_to_search: int) -> None:
+    """Find and click the day element after refreshing the page until it appears."""
     retry = 0
-    while elements == None and retry < 10:
-        driver.refresh()
-
-        WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.LINK_TEXT, str(day_to_search - 1)))
-        )
-        logging.info(f'Clicking {day_to_search} as the date')
-        elements = driver.find_elements(By.LINK_TEXT, str(day_to_search))
-        retry += 1
-
-    elements[0].click()
-
-    logging.info(f'Waiting for tee times to load')
-    WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.LINK_TEXT, str('18:56')))  # ToDo come up with something better than this
-    )
-    logging.info(f'Times loaded')
-
-    Item = namedtuple("teeTime", ["element", "time", "minutesFromIdeal"])
-    time_list = []
-
-    logging.info(f'Finding available times')
-    # Find available times
-    for tee_time in times_sorted_list:
-        # Attempt to find the element by ID
-        elements = driver.find_elements(By.LINK_TEXT, str(f"{tee_time.hour:02}") + ":" + str(f"{tee_time.minute:02}"))
-        if elements:
-            time_list.append(
-                Item(element=elements[0], time=str(f"{tee_time.hour:02}") + ":" + str(f"{tee_time.minute:02}"),
-                     minutesFromIdeal=abs(((tee_time - preferred_time).total_seconds()) / 60)))
-
-    logging.info(f'Ranking Times')
-    ranked_available_times = sorted(time_list, key=lambda x: x.minutesFromIdeal)
-
-    logging.info('Booking Best Time')
-    for time in ranked_available_times:
+    while retry < MAX_RETRIES:
         try:
-            time.element.click()  # Attempt to click on best time
-            # bookButton = WebDriverWait(driver, 20).until(
-            #    EC.element_to_be_clickable((By.NAME, "submit_frm_nopay"))
-            # )
-            # bookButton.click() #Click making booking
-            driver.switch_to.frame(0)
-            driver.find_element(By.NAME, "submit_frm_nopay").click()
-            logging.info(f'Successfully Booked the following time {time.time}')
-            break
+            driver.refresh()
+            logging.info(f'Refreshing page (Attempt {retry + 1} of {MAX_RETRIES})')
+
+            # Wait for the previous day's element to ensure the page is loaded
+            WebDriverWait(driver, TIMEOUT).until(
+                EC.element_to_be_clickable((By.LINK_TEXT, str(day_to_search - 1)))
+            )
+
+            logging.info(f'Clicking {day_to_search} as the date')
+            driver.find_element(By.LINK_TEXT, str(day_to_search)).click()
+            return  # Exit the function if the element is found and clicked
         except Exception as e:
-            logging.error(f"An unexpected error occurred: {e}")
+            logging.warning(f"Attempt {retry + 1} failed: {e}")
+            retry += 1
+            t.sleep(REFRESH_INTERVAL)
 
-    times = [item.time for item in ranked_available_times]
-    logging.info(f'Times available between {min_time} and {max_time}')
-    logging.info(times)
-    t.sleep(5)
+    raise Exception(f"Failed to find or click the day element after {MAX_RETRIES} retries")
 
-
-async def main():
-
+def book_preferred_time(driver: webdriver, time_to_book: str) -> None:
+    """Book the preferred tee time."""
     try:
-        logging.basicConfig(
-            level=logging.INFO,
-            format='%(asctime)s - %(levelname)s - %(message)s',
-            handlers=[
-                logging.FileHandler('app.log'),  # Log to a file
-                logging.StreamHandler(sys.stdout)  # Log to the console
-            ]
+        # Wait for the preferred time to be clickable
+        time_to_book_element = WebDriverWait(driver, TIMEOUT).until(
+            EC.element_to_be_clickable((By.LINK_TEXT, time_to_book))
         )
+        logging.info(f'Booking time: {time_to_book}')
+        time_to_book_element.click()
 
-        logging.info('Starting search for Stocky Tee Times')
-
-        username = "alexhicks"
-        password = "Spencer03"
-
-
-        ##eusername = "ejmulcahy"
-        ##epassword = "edge1982"
-
-
-        tasks = [
-            time_search(username, password, "09:04")
-            ##time_search(username, password, "9:20"),
-            ##time_search(username, password, "08:48"),
-            ##time_search(eusername, epassword, "09:12"),
-            ##time_search(eusername, epassword, "08:56"),
-            ##time_search(eusername, epassword, "9:28")
-        ]
-
-        results = await asyncio.gather(*tasks)
-
-
+        # Switch to the booking frame and submit the form
+        driver.switch_to.frame(0)
+        driver.find_element(By.NAME, "submit_frm_nopay").click()
+        logging.info(f'Successfully booked the following time: {time_to_book}')
     except Exception as e:
-        logging.error(e)
+        logging.error(f"Failed to book the preferred time: {e}")
+        raise
+
+def wait_and_book(driver: webdriver, start_time: datetime, day_to_search: int, time_to_book: str) -> None:
+    """Wait until the start time, find the day element, and book the preferred tee time."""
+    validate_inputs(start_time, day_to_search, time_to_book)
+    wait_until_start_time(start_time)
+    find_and_click_day_element(driver, day_to_search)
+    book_preferred_time(driver, time_to_book)
 
 
-asyncio.run(main())
+
+
+
+try:
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s',
+        handlers=[
+            logging.FileHandler('app.log'),  # Log to a file
+            logging.StreamHandler(sys.stdout)  # Log to the console
+        ]
+    )
+
+    logging.info('Starting search for Stocky Tee Times')
+
+    data = json.loads(sys.argv[1])
+    username = data.get('username')
+    password = data.get('password')
+    time_to_book = data.get('time_to_book')
+
+    login_and_setup(username, password, time_to_book)
+
+
+
+except Exception as e:
+    logging.error(e)
+
+
+
 
 
 
